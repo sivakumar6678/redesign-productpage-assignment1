7a34609fd8d74c564f057125df22d7db48c42814
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-04-28 23:06:47 +0530

    fix: update PublicRoute to navigate to unAuthenticatedEntryPath instead of Outlet for better routing

--- src/components/route/PublicRoute.tsx
+++ src/components/route/PublicRoute.tsx
@@ -3,11 +3,12 @@ import appConfig from '@/configs/app.config'
 import { useAuth } from '@/auth'
 
 const { authenticatedEntryPath } = appConfig
+const { unAuthenticatedEntryPath } = appConfig
 
 const PublicRoute = () => {
     const { authenticated } = useAuth()
 
-    return authenticated ? <Navigate to={authenticatedEntryPath} /> : <Outlet />
+    return authenticated ? <Navigate to={authenticatedEntryPath} /> : <Navigate to={unAuthenticatedEntryPath} />
 }
 
 export default PublicRoute
